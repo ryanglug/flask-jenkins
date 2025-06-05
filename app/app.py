391308efead41f443bb288c79ae183dd751fbe4b
glugnoob <r.gluger23@gmail.com>
2025-06-05 22:07:37 +0100

    added origin

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, supports_credentials=True)
+CORS(app, supports_credentials=True, origins=["http://localhost:1234"])
 
 
 @app.teardown_appcontext
